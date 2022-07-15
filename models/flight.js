5b8db2a770258950bcba152bff17a71054b05b69
abpandanguyen <abpandanguyen@gmail.com>
2022-07-14 17:14:26 -0700

    flightindex

---
+++ models/flight.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United'],
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            // whatever is returned is assigned
+            // to date created+1
+            return new Date().getFullYear()++;
+        }
+    }
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
